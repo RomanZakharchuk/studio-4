2f84c6edff3ee95cfe141cae8ca3dd9dcc3fc533
RomanZakharchuk <70903505+RomanZakharchuk@users.noreply.github.com>
2021-04-26 16:00:32 +0300

    add curousel

---
+++ js/carousel.js
@@ -0,0 +1,11 @@
+"use strict";
+
+$(document).ready(function () {
+  var owl = $('.owl-carousel');
+  owl.owlCarousel({
+    items: 3,
+    loop: true,
+    margin: 10,
+    autoplay: true
+  });
+});
